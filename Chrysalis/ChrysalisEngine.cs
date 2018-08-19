b426464103166848a050e878b94f0563db0cf9fb
erwijet <35789936+erwijet@users.noreply.github.com>
2018-08-19 10:52:20 -0500

    Add files via upload

---
+++ Chrysalis/ChrysalisEngine.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+
+namespace ChrysalisLib
+{
+    public class Chrysalis
+    {
+        const int MAX = 255;
+        internal static Color FOLDER_FLAG = Color.FromArgb(255, 255, 0, 254);
+
+        internal static Color GetColorFromByte(byte data)
+            => (data < MAX) ? Color.FromArgb(data, 0, 0, 0) :
+            (data - MAX < MAX) ? Color.FromArgb(MAX, data - MAX, 0, 0) :
+            (data - (2 * MAX) < MAX) ? Color.FromArgb(MAX, MAX, data - (2 * MAX), 0) :
+            Color.FromArgb(MAX, MAX, MAX, data - (3 * MAX));
+
+        internal static int GetValueOfColor(Color c) => c.A + c.R + c.G + c.B;
+
+        public static void DoFileDecode(FileInfo input)
+        {
+            string fileout = input.FullName.Substring(0, input.FullName.Length - ".png".Length);
+
+            if (!input.Exists)
+                return;
+            Bitmap b = (Bitmap)Image.FromFile(input.FullName);
+
+            int side = b.Size.Width;
+            Color firstPix = b.GetPixel(0, 0);
+            bool isFolder = firstPix == FOLDER_FLAG;
+
+            BinaryWriter bin = new BinaryWriter(File.Create(fileout + ((isFolder) ? ".gz" : "")));
+
+            for (int y = 0; y < side; y++)
+            {
+                for (int x = 0; x < side; x++)
+                {
+                    if (!(isFolder && x + y == 0))
+                    try { bin.Write((byte)GetValueOfColor(b.GetPixel(x, y))); bin.Flush(); }
+                    catch { }
+                }
+            }
+
+            bin.Close();
+            bin.Dispose();
+
+            if (isFolder)
+            {
+                DoFileDecompress(fileout + ".gz");
+                File.Delete(fileout + ".gz");
+            }
+        }
+
+        public static void DoFileDecompress(string file)
+        {
+            string newfile = file.Substring(0, file.Length - ".gz".Length) + ".zip";
+            FileStream fs = File.OpenRead(file);
+            GZipStream gzip = new GZipStream(fs, CompressionMode.Decompress);
+            Stream s = File.Create(newfile);
+
+            gzip.CopyTo(s);
+            s.Close();
+
+            gzip.Flush();
+            gzip.Close();
+
+            gzip.Dispose();
+            s.Dispose();
+
+            ZipFile.ExtractToDirectory(newfile, newfile.Substring(0, newfile.Length - ".zip".Length));
+            File.Delete(newfile);
+        }
+
+        public static void DoFolderEncode(DirectoryInfo folder)
+        {
+            string image = folder.FullName;
+
+            // Compress folder to a .gz file
+
+            FileInfo fi = new FileInfo(image);
+            ZipFile.CreateFromDirectory(folder.FullName, fi.FullName + ".zip");
+
+            GZipStream gzip = new GZipStream(File.Create(fi.FullName + ".gz"), CompressionMode.Compress);
+            Stream s = File.OpenRead(fi.FullName + ".zip");
+            s.CopyTo(gzip); s.Close();
+            gzip.Close();
+
+            // Encode the .gz file
+
+            DoFileEncode(new FileInfo(image + ".gz"), true);
+
+            // Cleanup
+
+            File.Delete(fi.FullName + ".zip");
+            File.Delete(image + ".gz");
+        }
+
+        public static void DoFileEncode(FileInfo input, bool folderFlag)
+        {
+            string image = input.FullName + ".png";
+
+            Bitmap bitmap = null;
+            if (!input.Exists)
+                return;
+
+            // Continue if file exists
+
+            byte[] data = File.ReadAllBytes(input.FullName);
+            int side = (int)Math.Ceiling(Math.Sqrt(data.Length));
+            bitmap = new Bitmap(side, side);
+
+            for (int y = 0; y < side; y++)
+            {
+                for (int x = 0; x < side; x++)
+                {
+                    Color pix;
+                    if (folderFlag && (x + y == 0))
+                        pix = FOLDER_FLAG; // Folder Flag Code
+                    else
+                    {
+                        try { pix = GetColorFromByte(data[((side * y) + x) - ((folderFlag) ? 1 : 0)]); }
+                        catch { pix = Color.FromArgb(255, 255, 255, 255); }
+                    }
+                    bitmap.SetPixel(x, y, pix);
+                }
+            }
+
+            bitmap.Save(image);
+        }
+    }
+}
---
+++ cenc/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChrysalisLib;
+
+namespace cenc
+{
+    class Program
+    {
+
+        static string HelpMenu =
+            @"
+Chrysalis Encoding Tool. Help Menu.
+
+cenc.exe and Chrysalis are avalible under the GNU AGPLv3
+
+The Chrysalis library is avalible at http://erwijet.github.com/Chrysalis
+The cenc.exe tool  is avalible at http://github.com/erwijet/cenc
+
+Chrysalis v1.0. 16 August 2018
+cenc.exe  v1.0. 16 August 2018
+
+Usage: cenc [mode] <input type> filename
+
+Modes:
+-e: Encode
+-d: Decode
+
+Input Types:
+-f: File
+-d: Directory / Folder
+
+Examples:
+cenc -e -f unencodedFile.txt
+cenc -d encodedFile.png
+";
+        static void Main(string[] args)
+        {
+            string cmdString = GetCommandString(args) ?? "NaN";
+            if (cmdString == "NaN")
+            {
+                Console.WriteLine(HelpMenu);
+                Console.ReadKey();
+                return;
+            }
+            if (cmdString.Substring(0, 1) == "+")
+            {
+                var flag = cmdString.Substring(1, 1);
+                if (flag == "%")
+                    Chrysalis.DoFileEncode(new System.IO.FileInfo(cmdString.Substring(2)), false);
+                else
+                    Chrysalis.DoFolderEncode(new System.IO.DirectoryInfo(cmdString.Substring(2)));
+            }
+            else
+            {
+                Chrysalis.DoFileDecode(new System.IO.FileInfo(cmdString.Substring(1)));
+            }
+            
+        }
+
+        static string GetCommandString(params string[] args)
+        {
+            string commandString = null;
+
+            switch (args.Length)
+            {
+                case 3:
+                    if (args[0] == "-e" && (args[1] == "-f" || args[1] == "-d") && args[2] != "")
+                        commandString = "+" + ((args[1] == "-f") ? "%" : "&") + args[2];
+                    break;
+                case 2:
+                    if (args[0] == "-d" && args[1] != "")
+                        commandString = "-" + args[1];
+                    break;
+            }
+
+            if (commandString != null)
+                return commandString;
+            return null;
+        }
+    }
+}
+
